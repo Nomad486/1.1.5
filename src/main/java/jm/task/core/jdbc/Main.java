9725cd7cb30a28c2441a700fc572edf488c84692
Nomad486 <168753020+Nomad486@users.noreply.github.com>
2024-05-15 22:12:30 +0300

    Задача исправлена

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 30);
+        userService.saveUser("Петр", "Петров", (byte) 25);
+        userService.saveUser("Сидор", "Сидоров", (byte) 20);
+        userService.saveUser("Николай", "Николаев", (byte) 35);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
